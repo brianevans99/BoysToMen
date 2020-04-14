5c0fa1d2604b167619305c21c157a34fb0a824ae
Brian Evans <brianevans99@gmail.com>
2020-04-14 09:24:29 -0400

    begin adding React components, working on Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Home from './pages/Home'
+import Afterschool from './pages/Afterschool'
+import NotFound from './pages/NotFound'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/afterschool' component={Afterschool} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </Fragment>
+  )
 }
 
 export default App
---
+++ client/src/pages/Afterschool.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class Afterschool extends Component {
+  render() {
+    return <div>afterschool program</div>
+  }
+}
+
+export default Afterschool
