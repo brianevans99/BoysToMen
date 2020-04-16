191aaae2e8f2e1b6185bc0ed74b9c6dfd57c8a7b
Brian Evans <brianevans99@gmail.com>
2020-04-16 08:35:11 -0400

    added additional verses per client's request

---
+++ client/src/data/CarouselData.js
@@ -0,0 +1,15 @@
+import image1 from '../assets/images/0425171750.jpg'
+import image2 from '../assets/images/0711170900a.jpg'
+import image3 from '../assets/images/0713171151c.jpg'
+import image4 from '../assets/images/0928171903a.jpg'
+import image5 from '../assets/images/1111171445b.jpg'
+import image6 from '../assets/images/1010171954.jpg'
+
+export default [
+  { id: 1, carouselImg: image1 },
+  { id: 2, carouselImg: image2 },
+  { id: 3, carouselImg: image3 },
+  { id: 4, carouselImg: image4 },
+  { id: 5, carouselImg: image5 },
+  { id: 6, carouselImg: image6 },
+]
