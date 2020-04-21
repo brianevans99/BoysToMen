f9b89d666ab3c207a050acc5157a72ace9d1af1d
Brian Evans <brianevans99@gmail.com>
2020-04-21 12:10:48 -0400

    added api.esv for rotating verses; added tie logo

--- client/src/components/TopCard.js
+++ client/src/components/TopCard.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Verse from '../components/Verse'
 import styled from 'styled-components'
+import tieLogo from '../assets/images/tieLogo.png'
 
 const Styles = styled.div`
   h2 {
@@ -11,6 +12,9 @@ const Styles = styled.div`
   }
   #verse {
     padding-top: 16px;
+  }
+  #logo {
+    max-height: 100px;
   }
 
   @media only screen and (max-width: 992px) {
@@ -26,11 +30,21 @@ const Styles = styled.div`
 const TopCard = () => (
   <Styles>
     <Row>
-      <Col lg={6}>
-        <h2 className='text-uppercase'>Boys To Men</h2>
-        <p className='lead'>
-          Guiding teenage boys in DeKalb County to be men of God...
-        </p>
+      <Col lg={5}>
+        <div className='d-flex flex-column justify-content-around'>
+          <h2 className='text-uppercase'>Boys To Men</h2>
+          <p className='lead'>
+            Guiding teenage boys in DeKalb County to be men of God...
+          </p>
+        </div>
+      </Col>
+      <Col lg={1} className='d-flex align-items-center'>
+        <img
+          src={tieLogo}
+          alt='Boys To Men | Atlanta Youth Project'
+          className='img-fluid'
+          id='logo'
+        />
       </Col>
       <Col id='verse' lg={6}>
         <Verse />
--- client/src/components/Verse.js
+++ client/src/components/Verse.js
@@ -3,7 +3,8 @@ import Request from 'axios-react'
 import spinner from '../assets/images/loadingSpinner.gif'
 import verseRef from '../data/verseRef'
 
-const url = 'https://bible-api.com/'
+// const url = 'https://bible-api.com/'
+const baseUrl = 'https://api.esv.org/v3/passage/text/?q='
 
 const r = Math.floor(Math.random() * verseRef.length)
 const backupVerse = 'I can do all things through Christ, who strengthens me.'
@@ -13,9 +14,17 @@ const Verse = () => (
   <Request
     config={{
       method: 'get',
-      url: `${url}${verseRef[r]}`,
+      url: `${baseUrl}${verseRef[r]}`,
+      headers: {
+        Authorization: '9c33d2c8646f6e5b0af69fd4f62d4230a1bbd0c4',
+      },
+      params: {
+        'include-passage-references': false,
+        'include-verse-numbers': false,
+        'include-first-verse-numbers': false,
+        'include-headings': false,
+      },
     }}
-    skip={false} // optional
   >
     {({ loading, response, error }) => (
       <Fragment>
@@ -27,8 +36,8 @@ const Verse = () => (
         {error && <span>{`${backupVerse} ~${backupRef}`}</span>}
         {response && (
           <div className='text-muted'>
-            <h6>{`${response.data.text}`}</h6>
-            <p>{`~${response.data.reference}`}</p>
+            <h6>{`${response.data.passages}`}</h6>
+            <p>{`~${response.data.query}`}</p>
           </div>
         )}
       </Fragment>
