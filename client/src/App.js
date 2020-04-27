1ca0dfc5e1e15a4dc9a99b86bd3e731b93f20567
Brian Evans <brianevans99@gmail.com>
2020-04-27 08:21:55 -0400

    console.log api key for production test

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Afterschool from './pages/Afterschool'
 import NotFound from './pages/NotFound'
 
 function App() {
+  console.log(process.env.API_KEY)
   return (
     <Fragment>
       <NavigationBar />
